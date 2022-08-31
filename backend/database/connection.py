c4bcc5a6296b409f2324c8b5614ba50eeb0fff8f
n4lkin <kalugin.serej1@gmail.com>
2022-08-31 14:28:12 +0300

    feat: now environments gets from Settings class

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -2,7 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@0.0.0.0/postgres"
+from settings import Settings
+
+POSTGRES_HOST = Settings.POSTGRES_HOST
+POSTGRES_DB = Settings.POSTGRES_DB
+POSTGRES_USER =  Settings.POSTGRES_USER
+POSTGRES_PASSWORD = Settings.POSTGRES_PASSWORD
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
