0ea7b03f1e50bcb42bf7832ed6c063f92147656c
n4lkin <kalugin.serej1@gmail.com>
2022-08-31 14:29:15 +0300

    del: AuthJWT

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,6 @@ import binascii
 
 from auction.views import router as auction_router
 from fastapi import FastAPI
-from fastapi_jwt_auth import AuthJWT
-from fastapi_jwt_auth.exceptions import AuthJWTException
-from settings import Settings
 from starlette.authentication import (
     AuthCredentials,
     AuthenticationBackend,
@@ -14,15 +11,8 @@ from starlette.authentication import (
 )
 from starlette.middleware import Middleware
 from starlette.middleware.authentication import AuthenticationMiddleware
-from starlette.requests import Request
-from starlette.responses import JSONResponse
 from user.authentication_logic import router as authentication_router
 from user.views import router as user_router
-
-
-@AuthJWT.load_config
-def get_config():
-    return Settings()
 
 
 class BasicAuthBackend(AuthenticationBackend):
@@ -54,15 +44,3 @@ app.include_router(authentication_router)
 
 app.include_router(user_router)
 app.include_router(auction_router)
-
-
-@app.exception_handler(AuthJWTException)
-def authjwt_exception_handler(request: Request, exc: AuthJWTException):
-    return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
-
-
-@app.middleware("http")
-async def create_cose(request: Request, call_next):
-    response = await call_next(request)
-    response.headers["user"] = "123"
-    return response
